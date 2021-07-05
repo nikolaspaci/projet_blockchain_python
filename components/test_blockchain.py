bb5f7d4ff254d3757c9b90324448980327215c40
nikolaspaci <nikolaspaci>
2021-07-05 11:46:56 +0200

    Ajout fichier json pour test_blockchain.py

--- components/test_blockchain.py
+++ components/test_blockchain.py
@@ -1,10 +1,14 @@
+import json
 from sys import setdlopenflags
 from ctypes import RTLD_GLOBAL
 setdlopenflags (RTLD_GLOBAL | 2)
 
 from bloc_component import Bloc
 from blockchain_component import Blockchain
-b=Bloc()
-b2=Bloc()
-blockchain=Blockchain(b.to_json())
-print(blockchain)
+f = open('blockChain.json')
+data = json.load(f)
+
+from bloc_component import Bloc
+from blockchain_component import Blockchain
+print(data)
+blocchain=Blockchain(data)
